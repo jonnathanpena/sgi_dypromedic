63a5da49876fc2029faabc415b9eec62024ccece
jonnathanpena <razaisger@hotmail.com>
2018-10-25 16:05:04 -0500

    Orden ASC menu Egresos

--- navbar.php
+++ navbar.php
@@ -45,11 +45,11 @@
 					Egresos 
 				</a>
 				<ul class="dropdown-menu">
-					<li><a href="proveedores.php">Proveedores</a></li>
-					<li><a href="caja_chica.php">Caja Chica</a></li>
 					<li><a href="banco.php">Banco</a></li>
+					<li><a href="caja_chica.php">Caja Chica</a></li>
 					<li><a href="compras.php">Compras</a></li>
 					<li><a href="libro_diario.php">Libros Diario</a></li>
+					<li><a href="proveedores.php">Proveedores</a></li>
 				</ul>
 			</li>
 
